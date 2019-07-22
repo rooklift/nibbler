15c6c9830993eb358b72813f4ace049dafa35884
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2019-07-22 14:49:16 +0100

    Revert "recycled clickers"
    This reverts commit 830794e3f6daa200819cbae6beda13dcde7fc3bb.

--- 80_info.js
+++ 80_info.js
@@ -17,9 +17,7 @@ function NewInfoHandler() {
 	ih.stderr_log = "";
 
 	ih.one_click_moves = New2DArray(8, 8);	// Array of possible one-click moves. Updated by draw_arrows().
-
 	ih.info_clickers = [];					// Elements in the infobox. Updated by draw_infobox().
-	ih.recycled_clickers = [];				// Prevent needless allocations. Not sure if worthwhile.
 
 	ih.last_drawn_version = null;
 	ih.last_drawn_highlight = null;
@@ -374,7 +372,6 @@ function NewInfoHandler() {
 		this.last_drawn_highlight_class = highlight_class;
 		this.last_drawn_searchmoves = Array.from(searchmoves);
 
-		this.recycled_clickers = this.recycled_clickers.concat(this.info_clickers);
 		this.info_clickers = [];
 
 		let substrings = [];
@@ -432,21 +429,11 @@ function NewInfoHandler() {
 					spanclass += " nobr";
 				}
 				substrings.push(`<span id="infobox_${clicker_index++}" class="${spanclass}">${nice_pv[i]} </span>`);
-
-				let clicker;
-
-				if (this.recycled_clickers.length > 0) {
-					clicker = this.recycled_clickers.pop();
-				} else {
-					clicker = Object.create(null);
-				}
-
-				clicker.move = info.pv[i];
-				clicker.is_start = i === 0;
-				clicker.is_end = i === nice_pv.length - 1;
-
-				this.info_clickers.push(clicker);
-
+				this.info_clickers.push({
+					move: info.pv[i],
+					is_start: i === 0,
+					is_end: i === nice_pv.length - 1,
+				});
 				colour = OppositeColour(colour);
 			}
 
