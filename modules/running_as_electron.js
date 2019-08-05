917605be5fab034ade22e65d5d403b3ff4950562
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2019-08-05 01:18:26 +0100

    simplify

--- modules/running_as_electron.js
+++ modules/running_as_electron.js
@@ -5,12 +5,5 @@ const path = require("path");
 // Is there not a better way? Perhaps some Electron API somewhere?
 
 module.exports = () => {
-	if (path.basename(process.argv[0]).toLowerCase() === "electron" ||
-		path.basename(process.argv[0]).toLowerCase() === "electron framework" ||
-		path.basename(process.argv[0]).toLowerCase() === "electron helper" ||
-		path.basename(process.argv[0]).toLowerCase() === "electron.exe") {
-
-		return true;
-	}
-	return false;
+	return path.basename(process.argv[0]).toLowerCase().includes("electron");
 };
