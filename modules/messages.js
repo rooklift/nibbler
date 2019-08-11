f59718c15c5c408bf549e68dc1df9ac813cf5c1f
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2019-08-11 16:05:23 +0100

    Update messages.js

--- modules/messages.js
+++ modules/messages.js
@@ -44,7 +44,7 @@ safely ignore them.`;
 
 
 exports.engine_not_present =
-`Engine not found. Please find the engine via the Engine menu. You might also \
+`Engine not set. Please find the engine via the Engine menu. You might also \
 need to locate the weights (neural network) file.`;
 
 
