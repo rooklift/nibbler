1507a8fb2fc3e4dd2b04f5dac5ade867ac566acf
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2019-08-15 06:56:47 +0100

    Update 42_perft.js

--- 42_perft.js
+++ 42_perft.js
@@ -5,13 +5,11 @@ function Perft(fen, depth) {
 		throw "Need FEN and depth";
 	}
 	let val = perft(LoadFEN(fen), depth, true)
-	console.log("Perft output...", val);
+	console.log("Total..........", val);
 }
 
 function perft(pos, depth, print_moves) {
-
 	let moves = pos.movegen();
-
 	if (depth === 1) {
 		return moves.length;
 	} else {
@@ -19,10 +17,15 @@ function perft(pos, depth, print_moves) {
 		for (let mv of moves) {
 			let val = perft(pos.move(mv), depth - 1, false);
 			if (print_moves) {
-				console.log(mv, val);
+				perft_print_move(pos, mv, val);
 			}
 			count += val;
 		}
 		return count;
 	}
+}
+
+function perft_print_move(pos, mv, val) {
+	let nice = pos.nice_string(mv);
+	console.log(`${mv + (mv.length === 4 ? " " : "")}   ${nice + " ".repeat(7 - nice.length)}`, val);
 }
