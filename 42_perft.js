031e5c14516e1256f1b0398398652260601323b6
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2019-08-15 06:17:27 +0100

    Update 42_perft.js

--- 42_perft.js
+++ 42_perft.js
@@ -17,7 +17,7 @@ function perft(pos, depth, print_moves) {
 	} else {
 		let count = 0;
 		for (let mv of moves) {
-			let val = perft(pos.move(mv), depth - 1);
+			let val = perft(pos.move(mv), depth - 1, false);
 			if (print_moves) {
 				console.log(mv, val);
 			}
