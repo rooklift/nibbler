887e6971ce72199d9c2576badd3dbb053dd0c3d6
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2020-04-16 20:40:26 +0100

    Update node_perft_test.js

--- misc/node_perft_test.js
+++ misc/node_perft_test.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const FEN = "Qr3knr/P1bp1p1p/2pn1q2/4p3/2PP2pB/1p1N1bP1/BP2PP1P/1R3KNR w BHbh - 0 1";
 const DEPTH = 5;
 
-const sources = ["../20_utils.js", "../30_point.js", "../40_position.js", "../41_fen.js", "../42_perft.js"];
+const sources = ["../20_utils.js", "../30_point.js", "../31_sliders.js", "../40_position.js", "../41_fen.js", "../42_perft.js"];
 
 let concat = "";
 
