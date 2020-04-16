b45cdc9b2a3081c31cd87cd47404fc3ddd0b8ac6
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2020-04-16 13:36:27 +0100

    Noting a decent test

--- 42_perft.js
+++ 42_perft.js
@@ -43,3 +43,20 @@ function perft_print_move(pos, mv, val) {
 	let nice = pos.nice_string(mv);
 	console.log(`${mv + (mv.length === 4 ? " " : "")}   ${nice + " ".repeat(7 - nice.length)}`, val);
 }
+
+/*
+
+Suggested test (slow):
+
+	Perft("Q1r2knr/P1bp1p1p/2pn1q2/4p3/2PP2pB/1p3bP1/BP2PP1P/2R1NKNR w CHch - 0 1", 5)
+
+Expect result 29731327
+
+In Stockfish:
+
+	setoption name UCI_Chess960 value true
+	ucinewgame
+	position fen Q1r2knr/P1bp1p1p/2pn1q2/4p3/2PP2pB/1p3bP1/BP2PP1P/2R1NKNR w CHch - 0 1
+	go perft 5
+
+*/
