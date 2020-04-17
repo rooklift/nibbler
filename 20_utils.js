f0e01c76facc7d38ea1d53f5448e7ca1d9e4069e
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2020-04-17 15:11:14 +0100

    Object.freeze()

--- 20_utils.js
+++ 20_utils.js
@@ -6,8 +6,8 @@ function XY(s) {				// e.g. "b7" --> [1, 1]
 		XY.cache = Object.create(null);
 		for (let x = 0; x < 8; x++) {
 			for (let y = 0; y < 8; y++) {
-				XY.cache[S(x, y).toLowerCase()] = [x, y];
-				XY.cache[S(x, y).toUpperCase()] = [x, y];
+				XY.cache[S(x, y).toLowerCase()] = Object.freeze([x, y]);
+				XY.cache[S(x, y).toUpperCase()] = Object.freeze([x, y]);
 			}
 		}
 	}
