094cff3edff418b585b07194daece87fbd149434
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2020-04-25 20:49:32 +0100

    Update 51_winrate.js

--- 51_winrate.js
+++ 51_winrate.js
@@ -13,32 +13,23 @@ function NewGrapher() {
 		}
 	};
 
-	grapher.draw = function(node) {
+	grapher.clear_pos_line = function() {
 
-		if (this.last_drawn_board === node.get_board() && performance.now() - grapher.last_draw_time < 500) {
-			return;
+		let top = document.getElementById("graph_pos_top");
+		let bottom = document.getElementById("graph_pos_bottom");
+
+		if (top && bottom) {
+			graph.removeChild(top);
+			graph.removeChild(bottom);
 		}
+	};
 
-		this.last_drawn_board = node.get_board();
-		this.last_draw_time = performance.now();
-
-		this.clear_graph();
+	grapher.draw = function(node) {
 
 		let width = graph.getBoundingClientRect().right - graph.getBoundingClientRect().left;
 		let height = graph.getBoundingClientRect().bottom - graph.getBoundingClientRect().top;
 
-		// Horizontal (50%) line...
-		this.add_line(0, height / 2, width, height / 2, "#666666", 1, true, true);
-
 		let eval_list = node.future_eval_history();
-
-		if (eval_list.length < 2) {
-			return;
-		}
-
-		let last_x = null;
-		let last_y = null;
-		let last_n = null;
 
 		let imaginary_length = 2048;
 
@@ -49,38 +40,59 @@ function NewGrapher() {
 			}
 		}
 
-		for (let n = 0; n < eval_list.length; n++) {
+		if (performance.now() - grapher.last_draw_time < 500) {
 
-			let e = eval_list[n];
+			this.last_drawn_board = node.get_board();
+			this.last_draw_time = performance.now();
 
-			if (e !== null) {
+			this.clear_graph();
 
-				let x = width * n / (imaginary_length);
-
-				let y = (1 - e) * height;
-				if (y < 1) y = 1;
-				if (y > height - 2) y = height - 2;
-
-				let interp = n - last_n !== 1;
-
-				if (last_x !== null) {
-					this.add_line(last_x, last_y, x, y, interp ? "#999999" : "white", 2, interp, false);
-				}
-
-				last_x = x;
-				last_y = y;
-				last_n = n;
+			if (eval_list.length < 2) {
+				return;
 			}
+
+			// Horizontal (50%) line...
+			this.add_line(0, height / 2, width, height / 2, "#666666", 1, true, true, null);
+
+			let last_x = null;
+			let last_y = null;
+			let last_n = null;
+
+			for (let n = 0; n < eval_list.length; n++) {
+
+				let e = eval_list[n];
+
+				if (e !== null) {
+
+					let x = width * n / (imaginary_length);
+
+					let y = (1 - e) * height;
+					if (y < 1) y = 1;
+					if (y > height - 2) y = height - 2;
+
+					let interp = n - last_n !== 1;
+
+					if (last_x !== null) {
+						this.add_line(last_x, last_y, x, y, interp ? "#999999" : "white", 2, interp, false, null);
+					}
+
+					last_x = x;
+					last_y = y;
+					last_n = n;
+				}
+			}
+		} else {
+			this.clear_pos_line();
 		}
 
 		// Vertical position line...
 		let depth = node.depth();
 		let colour = node.is_main_line() ? "#6cccee" : "#ffff00";
-		this.add_line(width * depth / imaginary_length, height / 2 - 3, width * depth / imaginary_length, 0, colour, 1, true, true);
-		this.add_line(width * depth / imaginary_length, height / 2 + 2, width * depth / imaginary_length, height, colour, 1, true, true);
+		this.add_line(width * depth / imaginary_length, height / 2 - 3, width * depth / imaginary_length, 0, colour, 1, true, true, "graph_pos_top");
+		this.add_line(width * depth / imaginary_length, height / 2 + 2, width * depth / imaginary_length, height, colour, 1, true, true, "graph_pos_bottom");
 	};
 
-	grapher.add_line = function(x1, y1, x2, y2, colour, stroke_width, dashed, crisp) {
+	grapher.add_line = function(x1, y1, x2, y2, colour, stroke_width, dashed, crisp, id) {
 		let element = document.createElementNS("http://www.w3.org/2000/svg", "line");
 		element.setAttributeNS(null, "x1", x1);
 		element.setAttributeNS(null, "y1", y1);
@@ -93,6 +105,9 @@ function NewGrapher() {
 		}
 		if (crisp) {
 			element.setAttributeNS(null, "shape-rendering", "crispEdges");
+		}
+		if (id) {
+			element.setAttributeNS(null, "id", id);
 		}
 		graph.appendChild(element);
 	};
