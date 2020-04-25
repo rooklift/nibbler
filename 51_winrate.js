b6a0e5bd75b7493d91ea1304b8e5282e0673d646
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2020-04-25 21:52:00 +0100

    lol

--- 51_winrate.js
+++ 51_winrate.js
@@ -42,7 +42,7 @@ function NewGrapher() {
 			}
 		}
 
-		if (performance.now() - grapher.last_draw_time < 500) {
+		if (performance.now() - grapher.last_draw_time > 500) {
 
 			this.last_drawn_board = node.get_board();
 			this.last_draw_time = performance.now();
