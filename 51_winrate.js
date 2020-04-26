5d52b9e007ccaa8317ea79fbc8c049a54403d761
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2020-04-26 11:11:04 +0100

    redo the grapher draw logic

--- 51_winrate.js
+++ 51_winrate.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// FIXME: click on graph to go to the move.
+
 function NewGrapher() {
 
 	let grapher = Object.create(null);
@@ -13,7 +15,7 @@ function NewGrapher() {
 		}
 	};
 
-	grapher.clear_pos_line = function() {
+	grapher.clear_position_line = function() {
 
 		let top = document.getElementById("graph_pos_top");
 		let bottom = document.getElementById("graph_pos_bottom");
@@ -24,80 +26,88 @@ function NewGrapher() {
 		}
 	};
 
-	grapher.draw = function(node, force) {
-
-		// FIXME: this is some tangled logic. Rewrite.
-
-		let boundingrect = graph.getBoundingClientRect();
-
-		let width = boundingrect.right - boundingrect.left;
-		let height = boundingrect.bottom - boundingrect.top;
-
-		let eval_list = node.future_eval_history();
-
-		let imaginary_length = 2048;
+	grapher.imaginary_length = function(real_length) {
 
 		for (let n of [128, 256, 512, 1024]) {
-			if (eval_list.length < n) {
-				imaginary_length = n;
-				break;
+			if (real_length <= n) {
+				return n;
 			}
 		}
+
+		return 2048;
+	};
+
+	grapher.draw = function(node, force) {
+
+		let boundingrect = graph.getBoundingClientRect();
+		let width = boundingrect.right - boundingrect.left;
+		let height = boundingrect.bottom - boundingrect.top;
+		let eval_list = node.future_eval_history();
 
 		if (force || performance.now() - grapher.last_draw_time > 500) {
-
-			this.last_drawn_board = node.get_board();
-			this.last_draw_time = performance.now();
-
-			this.clear_graph();
-
-			// Horizontal (50%) line...
-
-			this.add_line(0, height / 2, width, height / 2, "#666666", 1, true, true, null);
-
-			if (eval_list.length < 2) {
-				return;
-			}
-
-			let last_x = null;
-			let last_y = null;
-			let last_n = null;
-
-			for (let n = 0; n < eval_list.length; n++) {
-
-				let e = eval_list[n];
-
-				if (e !== null) {
-
-					let x = width * n / imaginary_length;
-
-					let y = (1 - e) * height;
-					if (y < 1) y = 1;
-					if (y > height - 2) y = height - 2;
-
-					let interp = n - last_n !== 1;
-
-					if (last_x !== null) {
-						this.add_line(last_x, last_y, x, y, interp ? "#999999" : "white", 2, interp, false, null);
-					}
-
-					last_x = x;
-					last_y = y;
-					last_n = n;
-				}
-			}
+			this.draw_everything(width, height, eval_list, node);
 		} else {
-			this.clear_pos_line();
+			this.clear_position_line();
+			this.draw_position_line(width, height, eval_list, node);
+		}
+	};
+
+	grapher.draw_everything = function(width, height, eval_list, node) {
+
+		this.last_drawn_board = node.get_board();
+		this.last_draw_time = performance.now();
+
+		this.clear_graph();
+
+		// Horizontal (50%) line...
+
+		this.add_line(0, height / 2, width, height / 2, "#666666", 1, true, true, null);
+
+		if (eval_list.length < 2) {
+			return;
 		}
 
-		// Vertical position line...
+		let imaginary_length = grapher.imaginary_length(eval_list.length);
 
-		if (eval_list.length >= 2) {
-			let depth = node.depth();
-			let colour = node.is_main_line() ? "#6cccee" : "#ffff00";
-			this.add_line(width * depth / imaginary_length, height / 2 - 3, width * depth / imaginary_length, 0, colour, 1, true, true, "graph_pos_top");
-			this.add_line(width * depth / imaginary_length, height / 2 + 2, width * depth / imaginary_length, height, colour, 1, true, true, "graph_pos_bottom");
+		let last_x = null;
+		let last_y = null;
+		let last_n = null;
+
+		for (let n = 0; n < eval_list.length; n++) {
+
+			let e = eval_list[n];
+
+			if (e !== null) {
+
+				let x = width * n / imaginary_length;
+
+				let y = (1 - e) * height;
+				if (y < 1) y = 1;
+				if (y > height - 2) y = height - 2;
+
+				let interp = n - last_n !== 1;
+
+				if (last_x !== null) {
+					this.add_line(last_x, last_y, x, y, interp ? "#999999" : "white", 2, interp, false, null);
+				}
+
+				last_x = x;
+				last_y = y;
+				last_n = n;
+			}
 		}
+
+		this.draw_position_line(width, height, eval_list, node);
+	};
+
+	grapher.draw_position_line = function(width, height, eval_list, node) {
+
+		let imaginary_length = grapher.imaginary_length(eval_list.length);
+		let depth = node.depth();
+		let colour = node.is_main_line() ? "#6cccee" : "#ffff00";
+
+		this.add_line(width * depth / imaginary_length, height / 2 - 3, width * depth / imaginary_length, 0, colour, 1, true, true, "graph_pos_top");
+		this.add_line(width * depth / imaginary_length, height / 2 + 2, width * depth / imaginary_length, height, colour, 1, true, true, "graph_pos_bottom");
 	};
 
 	grapher.add_line = function(x1, y1, x2, y2, colour, stroke_width, dashed, crisp, id) {
