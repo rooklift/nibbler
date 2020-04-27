519686e942d82477f07a8dec4fb53899963d0cde
Leandro Alvarez <lealgoz@gmail.com>
2020-04-27 19:40:59 -0400

    add app icon

--- main.js
+++ main.js
@@ -55,6 +55,7 @@ function startup() {
 		height: config.height,
 		backgroundColor: "#000000",
 		resizable: true,
+    icon: "nibbler.png",
 		show: false,
 		useContentSize: true,
 		webPreferences: {
