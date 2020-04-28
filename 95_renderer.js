6720cb19e0023be686b84aca4f125fe769613ecf
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2020-04-28 11:33:00 +0100

    this is too confusing

--- 95_renderer.js
+++ 95_renderer.js
@@ -631,10 +631,6 @@ function NewRenderer() {
 					if (info) {
 						this.node.update_eval_from_info(info);
 					}
-
-					if (config.selfplay || this.node.children.length === 0) {		// In human v computer mode, only play at leaf
-						this.move(tokens[1]);
-					}
 				}
 			}
 		}
