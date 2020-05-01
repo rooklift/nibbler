f14b554c2b22c2055789fe65e8f5fd0911527d59
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2020-05-01 11:33:33 +0100

    pull in basic tree manipulation functions

--- 71_tree_handler.js
+++ 71_tree_handler.js
@@ -10,4 +10,153 @@ function NewTreeHandler() {
 	handler.root = NewTree();
 	handler.node = handler.root;
 
+	handler.prev = function() {
+		if (this.node.parent) {
+			this.node = this.node.parent;
+			return true;
+		}
+		return false;
+	};
+
+	handler.next = function() {
+		if (this.node.children.length > 0) {
+			this.node = this.node.children[0];
+			return true;
+		}
+		return false;
+	};
+
+	handler.goto_root = function() {
+		if (this.node !== this.root) {
+			this.node = this.root;
+			return true;
+		}
+		return false;
+	};
+
+	handler.goto_end = function() {
+		let end = this.node.get_end();
+		if (this.node !== end) {
+			this.node = end;
+			return true;
+		}
+		return false;
+	};
+
+	handler.return_to_main_line = function() {
+
+		let main_line = this.root.future_history();
+		let history = this.node.history();
+
+		let node = this.root;
+
+		for (let n = 0; n < history.length; n++) {
+			if (main_line[n] !== history[n]) {
+				break;
+			}
+			if (node.children.length === 0) {
+				break;
+			}
+			node = node.children[0];
+		}
+
+		if (this.node !== node) {
+			this.node = node;
+			return true;
+		}
+		return false;
+	};
+
+	handler.promote_to_main_line = function() {
+
+		let node = this.node;
+		let changed = false;
+
+		while (node.parent) {
+			if (node.parent.children[0] !== node) {
+				for (let n = 1; n < node.parent.children.length; n++) {
+					if (node.parent.children[n] === node) {
+						node.parent.children[n] = node.parent.children[0];
+						node.parent.children[0] = node;
+						break;
+					}
+				}
+				changed = true;
+			}
+			node = node.parent;
+		}
+
+		if (changed) {
+			tree_version++;
+		}
+
+		return changed;
+	};
+
+	handler.delete_other_lines = function() {
+
+		let changed = this.promote_to_main_line();
+		let node = this.root;
+
+		while (node.children.length > 0) {
+			if (node.children.length > 1) {
+				node.children = node.children.slice(0, 1);
+				changed = true;
+			}
+			node = node.children[0];
+		}
+
+		if (changed) {
+			tree_version++;
+		}
+
+		return changed;
+	};
+
+	handler.delete_children = function() {
+
+		if (this.node.children.length > 0) {
+			for (let child of this.node.children) {
+				child.detach();
+			}
+			tree_version++;
+			return true;
+		}
+
+		return false;
+	};
+
+	handler.delete_node = function() {
+
+		if (!this.node.parent) {
+			return this.delete_children();
+		}
+
+		let parent = this.node.parent;
+		this.node.detach();
+		this.node = parent;
+		tree_version++;
+		return true;
+	};
+
+	handler.delete_siblings = function() {
+
+		let changed = false;
+
+		if (this.node.parent) {
+			for (let sibling of this.node.parent.children) {
+				if (sibling !== this.node) {
+					sibling.detach();
+					changed = true;
+				}
+			}
+		}
+
+		if (changed) {
+			tree_version++;
+		}
+
+		return changed;
+	};
+
 }
