314eccc6e7366826eacc222c6846a5b5b59c744a
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2020-05-03 16:14:13 +0100

    -hover

--- 80_info.js
+++ 80_info.js
@@ -337,7 +337,7 @@ function NewInfoHandler() {
 
 			if (leela_maybe_running === false) {
 				if (config.versus === "") {
-					status_string += `<span class="yellow">HALTED <span id="gobutton_clicker" class="gobutton">(Go)</span> </span>`;
+					status_string += `<span class="yellow">HALTED <span id="gobutton_clicker" class="gobutton">&lt; Go &gt;</span> </span>`;
 				} else {
 					status_string += `<span class="yellow">YOUR MOVE</span>`;
 				}
