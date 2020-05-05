00c3d4f220356eface1b48fdfab2b7f3d2e42a7a
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2020-05-05 22:22:01 +0100

    Update 80_info.js

--- 80_info.js
+++ 80_info.js
@@ -4,13 +4,12 @@ function NewInfoHandler() {
 
 	let ih = Object.create(null);
 
-	ih.stderr_phase = true;
-
+	ih.stderr_phase = true;						// Where we print stderr to infobox at start.
 	ih.ever_received_q = false;
 	ih.ever_received_multipv_2 = false;
 
-	ih.one_click_moves = New2DArray(8, 8);	// Array of possible one-click moves. Updated by draw_arrows().
-	ih.info_clickers = [];					// Elements in the infobox. Updated by draw_infobox().
+	ih.one_click_moves = New2DArray(8, 8);		// Array of possible one-click moves. Updated by draw_arrows().
+	ih.info_clickers = [];						// Elements in the infobox. Updated by draw_infobox().
 
 	ih.stderr_log = "";
 	ih.special_message = null;
