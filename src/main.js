9b58656bfbd36f0c93d48d830ee8d1c7b72d206f
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2020-07-21 22:48:36 +0100

    turn spellcheck off for Electron >= 8

--- src/main.js
+++ src/main.js
@@ -62,6 +62,7 @@ function startup() {
 			backgroundThrottling: false,
 			enableRemoteModule: true,
 			nodeIntegration: true,
+			spellcheck: false,
 			zoomFactor: 1 / electron.screen.getPrimaryDisplay().scaleFactor		// Unreliable, see https://github.com/electron/electron/issues/10572
 		}
 	});
