1c1a0a5c6adf9a35d32e0beb390783ef243c86ad
rooklift <16438795+rooklift@users.noreply.github.com>
2021-03-22 16:34:47 +0000

    Update 95_renderer.js

--- src/95_renderer.js
+++ src/95_renderer.js
@@ -640,7 +640,7 @@ function NewRenderer() {
 	renderer.open = function(filename) {
 		let buf;
 		try {
-			if (!fs.existsSync(filename) {		// Silently return in this case.
+			if (!fs.existsSync(filename)) {		// Silently return in this case.
 				return;
 			}
 			buf = fs.readFileSync(filename);
