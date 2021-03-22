58b0c3fcbb65e8ab0ffdd61799dc245c1a650f7a
rooklift <16438795+rooklift@users.noreply.github.com>
2021-03-22 23:45:37 +0000

    Update config_io.js

--- src/modules/config_io.js
+++ src/modules/config_io.js
@@ -120,7 +120,7 @@ exports.defaults = {
 	"show_engine_state": false,
 
 	"search_nodes": null,
-	"search_nodes_special": 4000,
+	"search_nodes_special": 10000,
 
 	"save_enabled": false,
 	"override_piece_directory": null,
