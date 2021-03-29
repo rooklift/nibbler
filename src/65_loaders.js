00e58aa861dc8bf31d4b861152d965bff04488d9
rooklift <16438795+rooklift@users.noreply.github.com>
2021-03-29 01:30:38 +0100

    rearrange

--- src/65_loaders.js
+++ src/65_loaders.js
@@ -134,6 +134,63 @@ function NewPGNBookLoader(filename, callback) {
 
 // ------------------------------------------------------------------------------------------------------------------------------
 
+function NewPGNFileLoader(filename, callback) {
+
+	let loader = Object.create(null);
+	loader.type = "pgn";
+
+	loader.callback = callback;
+	loader.msg = "Loading PGN...";
+	loader.buf = null;
+	loader.preparser = null;
+
+	loader.shutdown = function() {
+		this.callback = null;
+		this.msg = "";
+		this.buf = null;
+		if (this.preparser) {
+			this.preparser.shutdown();
+			this.preparser = null;
+		}
+	};
+
+	loader.load = function(filename) {
+		fs.readFile(filename, (err, data) => {
+			if (err) {
+				console.log(err);
+				this.shutdown();
+			} else if (this.callback) {					// We might already have aborted
+				this.buf = data;
+				this.continue();
+			}
+		});
+	};
+
+	loader.continue = function() {
+
+		if (!this.callback) {
+			return;
+		}
+
+		if (!this.preparser) {
+			this.preparser = NewPGNPreParser(this.buf, (games) => {
+				if (this.callback) {
+					let cb = this.callback; cb(games);
+				}
+				this.shutdown();
+			});
+		}
+
+		this.msg = this.preparser.msg;
+		setTimeout(() => {this.continue();}, 20);	// Just to update these messages.
+	};
+
+	setTimeout(() => {loader.load(filename);}, 0);
+	return loader;
+}
+
+// ------------------------------------------------------------------------------------------------------------------------------
+
 function NewPGNPreParser(buf, callback) {		// Cannot fail unless aborted.
 
 	let loader = Object.create(null);
@@ -257,63 +314,6 @@ function NewPGNPreParser(buf, callback) {		// Cannot fail unless aborted.
 	};
 
 	setTimeout(() => {loader.continue();}, 0);		// setTimeout especially required here since there's no async load() function in this one.
-	return loader;
-}
-
-// ------------------------------------------------------------------------------------------------------------------------------
-
-function NewPGNFileLoader(filename, callback) {
-
-	let loader = Object.create(null);
-	loader.type = "pgn";
-
-	loader.callback = callback;
-	loader.msg = "Loading PGN...";
-	loader.buf = null;
-	loader.preparser = null;
-
-	loader.shutdown = function() {
-		this.callback = null;
-		this.msg = "";
-		this.buf = null;
-		if (this.preparser) {
-			this.preparser.shutdown();
-			this.preparser = null;
-		}
-	};
-
-	loader.load = function(filename) {
-		fs.readFile(filename, (err, data) => {
-			if (err) {
-				console.log(err);
-				this.shutdown();
-			} else if (this.callback) {					// We might already have aborted
-				this.buf = data;
-				this.continue();
-			}
-		});
-	};
-
-	loader.continue = function() {
-
-		if (!this.callback) {
-			return;
-		}
-
-		if (!this.preparser) {
-			this.preparser = NewPGNPreParser(this.buf, (games) => {
-				if (this.callback) {
-					let cb = this.callback; cb(games);
-				}
-				this.shutdown();
-			});
-		}
-
-		this.msg = this.preparser.msg;
-		setTimeout(() => {this.continue();}, 20);	// Just to update these messages.
-	};
-
-	setTimeout(() => {loader.load(filename);}, 0);
 	return loader;
 }
 
