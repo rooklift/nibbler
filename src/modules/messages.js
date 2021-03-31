465c6898bb9f55935266bb1d384668462d8970ec
rooklift <16438795+rooklift@users.noreply.github.com>
2021-03-31 21:19:37 +0100

    Update messages.js

--- src/modules/messages.js
+++ src/modules/messages.js
@@ -63,9 +63,9 @@ exports.about_sizes =
 Nibbler's config file (which can be found via the Dev menu).`;
 
 
-exports.about_hashes =		// FIXME, what filename did we settle on?
-`You can set the Hash value directly via Nibbler's config files (which can be found via \
-the Dev menu).`;
+exports.about_hashes =
+`You can set the Hash value directly via Nibbler's engines.json file (which can be found \
+via the Dev menu).`;
 
 
 exports.thread_warning =
