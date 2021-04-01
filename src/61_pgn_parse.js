15c4d908bda3720dbff2c2b4f9e3604107691004
rooklift <16438795+rooklift@users.noreply.github.com>
2021-04-01 15:22:42 +0100

    rm debug

--- src/61_pgn_parse.js
+++ src/61_pgn_parse.js
@@ -47,8 +47,6 @@ function PreParsePGN(buf) {							// buf should be the buffer for a single game,
 
 			tagline = tagline.slice(1, -1);			// So now it's like:		Foo "bar etc"
 
-			console.log(tagline);
-
 			let quote_i = tagline.indexOf(`"`);
 
 			if (quote_i === -1) {					// This is never the case, given the s.endsWith(`"]`) test above.
