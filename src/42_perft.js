a75c9bb63650f127cd336a28ec2d4a943f8ab10d
rooklift <16438795+rooklift@users.noreply.github.com>
2022-01-31 15:23:20 +0000

    Revert "Update 42_perft.js"
    This reverts commit cc77037a036e743d7162c3b5cd9c5a162082a5e4.

--- src/42_perft.js
+++ src/42_perft.js
@@ -48,7 +48,6 @@ let perft_known_values = {
 	"1nr1nk1r/1b5B/p1p1qp2/b2pp1pP/3P2P1/P3P2N/1Pp2P2/BNR2KQR w CHch g6 0 1": [0, 28,  964, 27838,  992438,  30218648],
 	"Qr3knr/P1bp1p1p/2pn1q2/4p3/2PP2pB/1p1N1bP1/BP2PP1P/1R3KNR w BHbh - 0 1": [0, 31, 1122, 34613, 1253934,  40393041],
 	"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1":   [0, 48, 2039, 97862, 4085603, 193690690],
-	"r3k2r/8/8/3BB3/3bb3/8/8/R3K2R w KQkq - 0 1":                             [0, 44, 1708, 66291, 2503066,  94496634],
 };
 
 function Perft(fen, depth) {
