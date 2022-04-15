e21ec64a37edc7ad1b529fd352ad28e4a89db0b0
rooklift <16438795+rooklift@users.noreply.github.com>
2022-04-15 13:50:05 +0100

    accept with or without win argument

--- src/modules/alert_main.js
+++ src/modules/alert_main.js
@@ -1,6 +1,7 @@
 "use strict";
 
-// To be used in the main process only.
+// Exports a function we use to draw alert messages.
+// To be used in the main process only (so when the renderer needs to make an alert, it sends the message to main via IPC).
 
 const electron = require("electron");
 const stringify = require("./stringify");
@@ -13,8 +14,11 @@ if (Number.isNaN(major_version)) {
 
 let alerts_open = 0;
 
-module.exports = function(msg) {
+module.exports = function(...args) {							// Can be called as  (msg)  or as  (win, msg)
 
+	let win = (args.length < 2) ? undefined : args[0];
+	let msg = (args.length < 2) ? args[0]   : args[1];
+	
 	if (alerts_open >= 3) {
 		console.log(msg);
 		return;
@@ -22,17 +26,33 @@ module.exports = function(msg) {
 
 	alerts_open++;
 
-	if (major_version <= 5) {		// Old API. Providing a callback makes the window not block the process.
+	if (major_version <= 5) {
 
-		electron.dialog.showMessageBox({message: stringify(msg), title: "Alert", buttons: ["OK"]}, () => {
-			alerts_open--;
-		});
+		// Old API. Providing a callback makes the window not block the process...
+		// This is all rather untested.
 
-	} else {						// New promise-based API. Despite what older (pre-v12) docs say, I don't think this ever blocks.
+		if (win) {
+			electron.dialog.showMessageBox(win, {message: stringify(msg), title: "Alert", buttons: ["OK"]}, () => {
+				alerts_open--;
+			});
+		} else {
+			electron.dialog.showMessageBox({message: stringify(msg), title: "Alert", buttons: ["OK"]}, () => {
+				alerts_open--;
+			});
+		}
 
-		electron.dialog.showMessageBox({message: stringify(msg), title: "Alert", buttons: ["OK"]}).then(() => {
-			alerts_open--;
-		});
+	} else {
 
+		// New promise-based API. Shouldn't block the process...
+
+		if (win) {
+			electron.dialog.showMessageBox(win, {message: stringify(msg), title: "Alert", buttons: ["OK"]}).then(() => {
+				alerts_open--;
+			});
+		} else {
+			electron.dialog.showMessageBox({message: stringify(msg), title: "Alert", buttons: ["OK"]}).then(() => {
+				alerts_open--;
+			});
+		}
 	}
 };
