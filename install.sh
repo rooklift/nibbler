f965f51719eb57b684cd32f27fa969cd5814c934
Cyrill <cyrillkuettel@gmail.com>
2022-08-15 22:59:13 +0200

    Add install script for linux

---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+BASE_URL="https://github.com/rooklift/nibbler/"
+
+# compute the latest release number dynamically
+VERSION_NR=$(basename $(curl -fs -o/dev/null -w %{redirect_url} ${BASE_URL}/releases/latest))
+VERSION_NR_ONLY_DIGIT="${VERSION_NR:1}"
+URL="${BASE_URL}releases/download/${VERSION_NR}/nibbler-${VERSION_NR_ONLY_DIGIT}-linux.zip"
+
+cd ~/Downloads
+echo "Downloading the latest release from the github release page:"
+echo ${URL}
+wget -q -c "${URL}"
+if [ $? -eq 0 ]; then
+    echo "Successfully Downloaded Nibbler ${VERSION_NR_ONLY_DIGIT} "
+else
+    echo "Failed to Download Nibbler ${VERSION_NR_ONLY_DIGIT}. Exiting ..."
+    exit 1
+fi
+
+ZIP_NAME="nibbler-${VERSION_NR_ONLY_DIGIT}-linux.zip"
+FILE_NAME="nibbler-${VERSION_NR_ONLY_DIGIT}-linux"
+sudo unzip -qq ${ZIP_NAME} -d /opt/
+sudo chmod +x /opt/${FILE_NAME}/nibbler
+echo "Nibbler ${VERSION_NR} is installed in /opt/${FILE_NAME}"
+
+
+read -p "Do you want to create a Desktop Shortcut? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+   sudo mkdir -p /usr/local/share/applications
+
+cat <<EOF | sudo tee -a /usr/local/share/applications/nibbler.desktop >/dev/null
+[Desktop Entry]
+Type=Application
+Version=1.0
+Name=Nibbler
+Icon=/opt/${FILE_NAME}/resources/app/pieces/K.png
+Exec=/opt/${FILE_NAME}/nibbler
+Terminal=false
+StartupNotify=false
+Categories=Game;BoardGame;
+EOF
+fi
+
+
