90c1619808612e1dc8c40b77ddfb94f605a95ffb
Cyrill <cyrillkuettel@gmail.com>
2022-08-19 23:58:47 +0200

    various improvements

--- src/install.sh
+++ src/install.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-
+set -e
 BASE_URL="https://github.com/rooklift/nibbler/"
 
-# compute the latest release number dynamically
+if [ ! -x /usr/bin/curl ] ; then
+    # some extra check if curl is not installed at the usual place
+    command -v curl >/dev/null 2>&1 || { echo >&2 "Please install curl or set it in your path. Aborting."; exit 1; }
+fi
+
+# calculate the latest release number dynamically
 VERSION_NR=$(basename $(curl -fs -o/dev/null -w %{redirect_url} ${BASE_URL}/releases/latest))
+
 VERSION_NR_ONLY_DIGIT="${VERSION_NR:1}"
 URL="${BASE_URL}releases/download/${VERSION_NR}/nibbler-${VERSION_NR_ONLY_DIGIT}-linux.zip"
 
-cd ~/Downloads
-echo "Downloading the latest release from the github release page:"
+cd /tmp
+echo "Downloading the latest release from the github release page ..."
 echo ${URL}
 wget -q -c "${URL}"
 if [ $? -eq 0 ]; then
@@ -21,14 +27,18 @@ fi
 ZIP_NAME="nibbler-${VERSION_NR_ONLY_DIGIT}-linux.zip"
 FILE_NAME="nibbler-${VERSION_NR_ONLY_DIGIT}-linux"
 LOCATION="/opt/${FILE_NAME}"
-echo "Unzipping to $LOCATION"
+echo "Unzipping to $LOCATION, sudo needed"
+echo sudo unzip -qq ${ZIP_NAME} -d /opt/
 sudo unzip -qq ${ZIP_NAME} -d /opt/
 sudo chmod +x /opt/${FILE_NAME}/nibbler
+echo "Successfully extracted Nibbler."
 
-read -p "Do you want to create a Desktop Shortcut? (y/n)" -n 1 -r
+read -p "Would you like to create a Desktop shortcut? (y/n)" -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-   sudo mkdir -p /usr/local/share/applications
+printf "\n"
+echo sudo mkdir -p /usr/local/share/applications
+sudo mkdir -p /usr/local/share/applications
 cat <<EOF | sudo tee -a /usr/local/share/applications/nibbler.desktop >/dev/null
 [Desktop Entry]
 Type=Application
@@ -40,11 +50,13 @@ Terminal=false
 StartupNotify=false
 Categories=Game;BoardGame;
 EOF
-  printf "\n"
-  echo "Desktop Shortcut created"
+printf "Desktop shortcut created:/usr/local/share/applications/nibbler.desktop"
+printf "\nThe Desktop shortcut will appear shortly in the applications menu."
+else
+	printf "\nNo Desktop shortcut"
 fi
 printf "\n"
-echo "Successfully installed Nibbler ${VERSION_NR} to /opt/${FILE_NAME}"
-echo "The Desktop Shortcut will appear shortly in the Applications menu."
+echo "Successfully installed Nibbler ${VERSION_NR_ONLY_DIGIT}"
+
 
 
