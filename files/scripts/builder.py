ae80f640cf904d221f98d8724a407e59300aea4d
rooklift <16438795+rooklift@users.noreply.github.com>
2022-12-13 13:30:59 +0000

    Update builder.py

--- files/scripts/builder.py
+++ files/scripts/builder.py
@@ -1,29 +1,32 @@
 import json, os, shutil, zipfile
 
 zips = {
-	"windows": "electron_zipped/electron-v9.4.4-win32-x64.zip",
-	"linux": "electron_zipped/electron-v9.4.4-linux-x64.zip",
+	"windows": "scripts/electron_zipped/electron-v9.4.4-win32-x64.zip",
+	"linux": "scripts/electron_zipped/electron-v9.4.4-linux-x64.zip",
 }
 
 # To build Nibbler: (for info see https://electronjs.org/docs/tutorial/application-distribution)
 #
 # Obtain the appropriate Electron asset named above, from https://github.com/electron/electron/releases
-# Create a folder called ./electron_zipped and place the Electron asset in it
+# Create a folder at scripts/electron_zipped and place the Electron asset in it
 # Run ./builder.py
+
+os.chdir("..")
 
 with open("package.json") as f:
 	version = json.load(f)["version"]
 
-useful_files = [file for file in os.listdir() if file.endswith(".js") or file.endswith(".html") or file.endswith(".css") or file == "package.json"]
-folders = ["modules", "pieces", "res"]
+useful_root_files = [file for file in os.listdir() if file.endswith(".js") or file.endswith(".html") or file.endswith(".css") or file == "package.json"]
+folders = ["js_modules", "js_renderer", "pieces", "res"]
 
 for key, value in zips.items():
 	if not os.path.exists(value):
+		print("{} not present!".format(value))
 		continue
-	build_dir = "dist/nibbler-{}-{}".format(version, key)
+	build_dir = "scripts/dist/nibbler-{}-{}".format(version, key)
 	build_app_dir = os.path.join(build_dir, "resources/app")
 	os.makedirs(build_app_dir)
-	for file in useful_files:
+	for file in useful_root_files:
 		shutil.copy(file, build_app_dir)
 	for folder in folders:
 		shutil.copytree(folder, os.path.join(build_app_dir, folder))
